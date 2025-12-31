b4be48e87ae254a8fb19b395951d9f04a74c5046
Nueva Digital Solution <nuevadigital.india@gmail.com>
2025-12-31 23:19:14 +0530

    Changes done

--- includes/widgets/class-nueva-cfrd-widget-accordion.php
+++ includes/widgets/class-nueva-cfrd-widget-accordion.php
@@ -27,6 +27,65 @@ class Nueva_CFRD_Widget_Accordion extends Nueva_CFRD_Widget_Base
 
     protected function add_layout_controls()
     {
-        // Add Accordion specific controls
+        // Behavior Controls
+        $this->add_control(
+            'accordion_behavior',
+            [
+                'label' => esc_html__('Behavior', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'default' => 'toggle',
+                'options' => [
+                    'toggle' => 'Toggle (Multiple Open)',
+                    'accordion' => 'Accordion (One Open)',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'default_state',
+            [
+                'label' => esc_html__('Default State', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'default' => 'all_closed',
+                'options' => [
+                    'all_closed' => 'All Closed',
+                    'first_open' => 'First Open',
+                ],
+            ]
+        );
+
+        // Styling Controls (Title & Content)
+        $this->add_control(
+            'accordion_title_color',
+            [
+                'label' => esc_html__('Title Color', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .nueva-accordion-title' => 'color: {{VALUE}}',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'accordion_title_bg',
+            [
+                'label' => esc_html__('Title Background', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .nueva-accordion-title' => 'background-color: {{VALUE}}',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'accordion_content_bg',
+            [
+                'label' => esc_html__('Content Background', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .nueva-accordion-content' => 'background-color: {{VALUE}}',
+                ],
+            ]
+        );
     }
 }
--- includes/widgets/class-nueva-cfrd-widget-slider.php
+++ includes/widgets/class-nueva-cfrd-widget-slider.php
@@ -27,7 +27,49 @@ class Nueva_CFRD_Widget_Slider extends Nueva_CFRD_Widget_Base
 
     protected function add_layout_controls()
     {
-        // Add Slider specific controls (autoplay, speed, etc)
-        // These keys would need to be handled by the JS script or Renderer
+        $this->add_control(
+            'slider_autoplay',
+            [
+                'label' => esc_html__('Autoplay', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'default' => 'yes',
+            ]
+        );
+
+        $this->add_control(
+            'slider_speed',
+            [
+                'label' => esc_html__('Speed (ms)', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::NUMBER,
+                'default' => 3000,
+            ]
+        );
+
+        $this->add_control(
+            'slider_loop',
+            [
+                'label' => esc_html__('Infinite Loop', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'default' => 'yes',
+            ]
+        );
+
+        $this->add_control(
+            'slider_arrows',
+            [
+                'label' => esc_html__('Show Arrows', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'default' => 'yes',
+            ]
+        );
+
+        $this->add_control(
+            'slider_dots',
+            [
+                'label' => esc_html__('Show Dots', 'nueva-cfrd'),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'default' => 'yes',
+            ]
+        );
     }
 }
